580869aaea1984eab9bd3eb1dc0d313e7bbc41d5
bsnisar <bogdan.sns@gmail.com>
2024-04-27 11:51:59 -0700

    next changes

---
+++ components/Logo.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1 class="font-heading text-4xl sm:text-5xl md:text-5xl xl:text-13xl tracking-tightest">RE:DRESS</h1>
+</template>
+
+
+<script>
+</script>
+
+<style>
+    .slide-up-enter {
+        transform: translateY(100%);
+        opacity: 0;
+    }
+
+    .slide-up-enter-active {
+        transform: translateY(0);
+        opacity: 1;
+        transition: transform 0.5s ease-out, opacity 0.5s ease-out;
+    }
+
+    .slide-up-exit {
+        transform: translateY(0);
+        opacity: 1;
+    }
+
+    .slide-up-exit-active {
+        transform: translateY(-100%);
+        opacity: 0;
+        transition: transform 0.5s ease-out, opacity 0.5s ease-out;
+    }
+
+</style>
