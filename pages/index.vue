28226c0538dc000c4bff734532d545abcd24ce0e
bsnisar <bogdan.sns@gmail.com>
2024-05-03 15:44:43 -0700

    next changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,55 +2,113 @@
 
     <div class="container mx-auto py-8 h-[90%]">
 
-        <section class="container mx-auto px-4" data-path="0.1">
-            <div class="relative mx-auto mb-8 pt-8 pb-8 xs:pb-8 lg:pb-8 flex justify-center">
+        <section class="container mx-auto px-4 mb-16 " data-path="0.1">
+            <div class="relative mx-auto mb-0 pt-8 pb-8  flex justify-center">
                 <img src="public/logo-long-white.png" class="h-8 me-3" alt="Redress Logo" />
             </div>
         </section>
 
-        <article class="md:mx-14 md:my-1 p-5 md:border">
-            <section class="mb-12 mb-24">
-                <h2 class="text-4xl font-semibold text-black mb-5">What Our Goal</h2>
-                <p class="text-xl">We are building productized “retrieval as a service” that focus on specific applications or modalities powering <strong>fashion</strong> industry at the beginning</p>
-            </section>       
-            
-            <section class="mb-24">
-                <h2 class="text-4xl font-semibold text-black mb-5">Who We Are</h2>
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-11" >
-                    <section class="h-full">
-                        <img class="rounded-full w-20 h-20 mb-3" src="public/linkedin/1707388155214.jpeg"/>
-                        <h5 class="text-lg font-medium	mb-1">Anastasia Demchuk</h5>
-                        <p>10 y. of business development in fashion</p>
-                        <p class="text-sm text-gray-400 hover:text-gray-700 mt-2">ex-Chanel</p>
-                        <div class="mt-2">
-                            <a href="https://www.linkedin.com/in/anastasia-demchuk/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+        <article class="md:mx-5 md:my-1 p-5 ">
+
+            <section class="block">
+                <h2 class="text-4xl font-semibold text-black my-16 text-center">Team</h2>
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-11" >
+                    <section class="team-item">
+                        <div class="team-elem">
+                            <img class="rounded-full w-20 h-20 mb-3" src="public/linkedin/1707388155214.jpeg"/>
+                        </div>
+                        <div class="team-elem mt-5">
+                            <h5 class="text-xl md:text-2xl font-medium	mb-1">Anastasia Demchuk</h5>
+                        </div>
+                        <div class="team-elem">
+                            <h5 class="text-sm mb-1 text-gray-400">business girl</h5>
+                        </div>                           
+                        <div class="">
+                            <hr class="my-4 mx-4" />
+                            <div class="team-elem">
+                                <p class="text-center">10 y. of business development in fashion</p>
+                            </div>
+                            <div class="mt-5 team-elem ">
+                                <!-- <p class="text-sm text-gray-400 hover:text-gray-700 mt-2">ex-Chanel</p> -->
+                                <div class="mt-2">
+                                    <a href="https://www.linkedin.com/in/anastasia-demchuk/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+                                </div>   
+                            </div>                            
                         </div>
                     </section>
-                    <section class="h-full">
-                        <img class="rounded-full w-20 h-20 mb-3" src="public/linkedin/1712726981259.jpeg"/>
-                        <h5 class="text-lg font-medium	mb-1">Bohdan Snisar</h5>
-                        <p>have been doing software development for 12 y.</p>
-                        <p class="text-sm text-gray-400 hover:text-gray-700 mt-2">ex-Revolut; ex-Wix</p>
-                        <div class="mt-2">
-                            <a href="https://www.linkedin.com/in/bsnisar/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+                    
+                    <section class="team-item">
+                        <div class="team-elem">
+                            <img class="rounded-full w-20 h-20 mb-3" src="public/linkedin/1712726981259.jpeg"/>
                         </div>
+                        <div class="team-elem mt-5">
+                            <h5 class="text-xl md:text-2xl font-medium	mb-1">Bohdan Snisar</h5>
+                        </div>
+                        <div class="team-elem">
+                            <h5 class="text-sm  mb-1 text-gray-400">tech guy</h5>
+                        </div>                        
+                        <div class="">
+                            <hr class="my-4 mx-4" />
+                            <div class="team-elem">
+                                <p class="text-center">Software engineering lead with 12 y. of expririence</p>
+                            </div>                        
+                            <div class="mt-5 team-elem ">
+                                <!-- <p class="text-sm text-gray-400 hover:text-gray-700 mt-2">ex-Revolut; ex-Wix</p> -->
+                                <div class="mt-2">
+                                    <a href="https://www.linkedin.com/in/bsnisar/"><svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+                                </div>                            
+                            </div>
+                        </div>
+
                     </section>
-                    <section class="h-full">
-                        <img class="rounded-full w-20 h-20 mb-3" src="https://placehold.co/400x400"/>
-                        <h5 class="text-lg font-medium mb-1">ABC ABC</h5>
-                        <p>loren ipsum dollar</p>
-                        <p>loren ipsum dollar</p>
+                    <section class="team-item">
+                        <div class="team-elem">
+                            <img class="rounded-full w-20 h-20 mb-3 grayscale" src="public/linkedin/1713993278780.jpeg"/>
+                        </div>
+                        <div class="team-elem mt-5">
+                            <h5 class="text-xl md:text-2xl font-medium	mb-1">Alisa Vinogradova</h5>
+                        </div>
+                        <div class="team-elem">
+                            <h5 class="text-sm  mb-1 text-gray-400">ML girl & scientist</h5>
+                        </div>                        
+                        <div class="">
+                            <hr class="my-4 mx-4" />
+                            <div class="team-elem">
+                                <p class="text-center">Deep Learning Engineer and Scientist with about 6 years</p>
+                            </div>                                                  
+                            <div class="mt-5 team-elem ">
+                                <div class="mt-2 mr-4">
+                                    <a href="https://www.linkedin.com/in/alisagazizullina/"><svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
+                                </div>       
+                                <div class="mt-2">
+                                    <a href="https://scholar.google.com/citations?user=W_313ocAAAAJ"><img class="w-8 h-8" src="public/icons8-google-scholar-48.png"/> </a>
+                                </div>                                                       
+                            </div>
+                        </div>
+
                     </section>
                 </div>
             </section>  
             
-            <section class="mb-12">
-                <h2 class="text-4xl font-semibold text-black mb-5">Incorporated At</h2>
-                <p>REDRESS SPACE LTD</p>
-                <p>Parker Russell, Level 30 The Leadenhall Building, 122 Leadenhall Street</p>
-                <p>London, UK, EC3V 4AB</p>
-            </section>   
+            <!-- <section class="block">
+                <h2 class="text-4xl font-semibold text-black my-16 text-center">Advisors</h2>
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-11" >
+                </div>
+            </section>   -->
+          
+            <section class="block">
+                <h2 class="text-4xl font-semibold text-black text-center">Have a question?</h2>
+                <h5 class="mt-7 mb-7 text-xl text-center text-gray-400">Feel free to text us</h5>
+                <div class="w-1/8 team-elem">
+                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900  group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <span class="text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white   group-hover:bg-opacity-0">
+                        <!-- <a href="/contact">Contact</a> -->
+                        </span>
+                    </button>
+                </div>
+            </section>  
                         
+                               
         </article>
 
     </div>
@@ -59,38 +117,25 @@
     <footer class="bg-white mt-8 mb-2">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div class="md:flex md:justify-between">
-                <div class="mb-6 md:mb-0">
-                    <a href="https://redress.space/" class="flex items-center mb-3">
-                        <img src="public/logo-long-white.png" class="h-8 me-3" alt="Redress Logo" />
-                    </a>
+                <div class="mb-0 md:mb-0">
 
                     <div class="text-sm text-gray-400 hover:text-gray-700   md:ms-5 md:mt-4">
-                       <p>© 2024. All Rights Reserved.</p>
-                       <p>Parker Russell, Level 30 The Leadenhall Building, 122 Leadenhall Street</p>
-                       <p>London, UK, EC3V 4AB</p>
+                       <p class="mb-2">© 2024. All Rights Reserved.</p>
                        <p>REDRESS SPACE LTD</p>
+                       <p>Parker Russell, Level 30 The Leadenhall Building</p>
+                       <p>122 Leadenhall Str.</p>
+                       <p>London, UK, EC3V 4AB</p>
                     </div>
                 </div>
 
                 <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                    <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">Resources
-                        </h2>
-                        <ul class="text-gray-500 font-medium">
-                            <li class="mb-4">
-                                <a href="https://www.redress.space" class="hover:underline">RE:DRESS</a>
-                            </li>
-                        </ul>
-                    </div>
+                    
                     <div>
                         <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">Follow us
                         </h2>
                         <ul class="text-gray-500 font-medium">
                             <li class="mb-4">
                                 <a href="https://www.instagram.com/redress.space?utm_source=qr" class="hover:underline ">Instagram</a>
-                            </li>
-                            <li>
-                                <a href="https://discord.gg/7G5N3n6U" class="hover:underline">Discord</a>
                             </li>
                             
                         </ul>
@@ -115,5 +160,26 @@
 </template>
 
 
-<style>
+<style scoped>
+    .block {
+        @apply mb-60;
+    }
+
+    .team-item {
+        @apply  py-4 md:px-0 lg:px-8 md:border;
+    }
+
+    .team-elem {
+        @apply flex justify-center;
+    }
+
+    .btn {
+        @apply font-bold py-2 px-4 rounded;
+    }
+    .btn-blue {
+        @apply bg-blue-500 text-white;
+    }
+    .btn-blue:hover {
+        @apply bg-blue-700;
+    }    
 </style>
