304069f22e4a4df8c708281195be09aef014583b
bsnisar <bogdan.sns@gmail.com>
2024-08-30 01:20:50 +0100

    add Contact Us

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,32 +3,22 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (<>
-  
-  <div className='flex'>
-            <Link href={'/terms'}><span className='font-medium text-gray-800 hover:underline'>Terms</span></Link>
-            <div className='px-3'>|</div>
-            <Link href={'/privacy'}><span className='font-medium text-gray-800 hover:underline'>Privacy</span></Link>
-          </div>
-  </>);
-  return (
-    <div className=' py-2 mt-0'>
-        <div className='container'>
-      <div className='w-9/12 md:w-full mx-auto md:flex justify-between py-10 md:py-24'>
-        <div className='md:w-2/5'>
-            <p className=' font-alexandria font-extralight text-[14px] leading-[17.07px] '>The Leadenhall Building, <br />
-            122 Leadenhall Street, City Of London, London,<br />United Kingdom, EC3V 4AB <br />info@redress.space</p>
-        </div>
-        <div className='flex justify-center md:justify-end items-center gap-4 md:w-2/6 py-6'>
-            <img src='/assets/facebook.svg' alt="Facebook"  className='cursor-pointer h-[37px] w-[37px]' />
-            <img src='/assets/instagram.svg' alt="Facebook" className='cursor-pointer h-[41px] w-[41px]' />
-            <img src='/assets/linkedin.svg' alt="Facebook"  className='cursor-pointer h-[41px] w-[41px]'/>
-        </div>
+    <div className='flex justify-between'>
+      <div className='mb-3'>
+        <a className='font-medium text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-200 hover:underline' href='mailto:info@redress.space'>Contact Us</a>
       </div>
-          <p className='text-center mt-4 text-[10px] md:text-[14px] md:leading-[17px] leading-3  font-alexandria font-normal md:pb-5'>Copyrighted © 2024 Design and upload by ReDress</p>
+
+      <div className='flex text-sm '>
+        <Link href={'/terms'}><span className='font-medium text-gray-600 dark:text-white hover:underline '>Terms</span></Link>
+        <div className='px-3 dark:text-white'>|</div>
+        <Link href={'/privacy'}><span className='font-medium text-gray-600 dark:text-white hover:underline '>Privacy</span></Link>
       </div>
+
+
     </div>
-    
-  )
+
+  </>);
+
 }
 
 export default Footer
