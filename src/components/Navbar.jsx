d6dd1be5af24aba2717db7b6f130f7ba7777b89c
bsnisar <bogdan.sns@gmail.com>
2024-09-02 11:20:42 +0100

    fix spelling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({ toggleTheme, theme }) => {
 
   return (
     <div className="container py-2 px-28 pl-5 md:flex justify-between px-0">
-      <h5 className="text-xl">Match shoppers with clohtes they need</h5>
+      <h5 className="text-xl">Match shoppers with clothes they need</h5>
     </div>
   );
 
