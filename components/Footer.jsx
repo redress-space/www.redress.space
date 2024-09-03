dcd7e72eb6daffa06dd862e3209ae41635b8e776
bsnisar <bogdan.sns@gmail.com>
2024-09-03 21:04:06 +0100

    minor style changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
   return (<>
     <div className='flex justify-between'>
       <div className='mb-3'>
-        <a className='font-medium text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-200 hover:underline' href='mailto:info@redress.space'>Contact Us</a>
+        <a className='font-medium text-black hover:text-gray-600  dark:hover:text-gray-200 hover:underline' href='mailto:info@redress.space'>Contact Us</a>
       </div>
 
       <div className='flex text-sm '>
-        <Link href={'/terms'}><span className='font-medium text-gray-600 dark:text-white hover:underline '>Terms</span></Link>
-        <div className='px-3 dark:text-white'>|</div>
-        <Link href={'/privacy'}><span className='font-medium text-gray-600 dark:text-white hover:underline '>Privacy</span></Link>
+        <Link href={'/terms'}><span className='font-medium text-gray-600  hover:underline '>Terms</span></Link>
+        <div className='px-3 '>|</div>
+        <Link href={'/privacy'}><span className='font-medium text-gray-600  hover:underline '>Privacy</span></Link>
       </div>
     </div>
   </>);
