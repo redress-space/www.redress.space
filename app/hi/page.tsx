99bd9b45af0c28108b16e607827bf81806e0f8d8
bsnisar <bogdan.sns@gmail.com>
2024-09-05 13:50:35 +0100

    fix build

--- app/hi/page.tsx
+++ app/hi/page.tsx
@@ -60,10 +60,10 @@ export default function WaitlistPage() {
         </div>
 
         <div className="text-lg">
-          <p className="font-bold mb-2"> You’re our {verificationMessage}th shopping insider in line 🎉</p>
+          <p className="font-bold mb-2"> You&apos;re our {verificationMessage}th shopping insider in line 🎉</p>
           <p>
             Thank you for joining the waitlist! We&apos;ll keep you posted and make
-            sure you're the first to know when we are ready. Stay tuned!
+            sure you&apos;re the first to know when we are ready. Stay tuned!
           </p>
         </div>
       </div>
