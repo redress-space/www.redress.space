9657fd454c8da14ea39ffd44556156bf28acb20e
bsnisar <bogdan.sns@gmail.com>
2024-09-05 17:51:49 +0100

    Add svg as logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import Image from 'next/image';
 
-import logo from "@/public/logo.svg";
+import RedressLogo from "@/public/logo.svg";
 
 const Logo = () => {
   return (<>
     <div className=''>
-        <Image
-        priority
-        src={logo}
-        // height={32}
-        // width={32}
-        alt="Redress"
-        />
+        <RedressLogo />
     </div>
   </>);
 
--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  }
+};
 
 export default nextConfig;
