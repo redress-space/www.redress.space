40f8d9a2e8013ced852dec5ffd6b8d7d79508ae5
bsnisar <bogdan.sns@gmail.com>
2024-09-09 21:06:56 +0100

    update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <div className="container py-2 pb-8 px-28 pl-0 md:flex justify-between px-0">
       <h5 className="text-xl">Matching shoppers with clothes they need</h5>
       <div>
-        <h2 className="font-2xl text-bold">
+        {/* <h2 className="font-2xl text-bold">
           <Link href={"/about"}>About</Link>
-        </h2>
+        </h2> */}
       </div>
     </div>
   );
